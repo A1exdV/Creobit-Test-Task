8ecca7a729d2ac5b54013ff48c0273ad30bd110a
AlexdV <alexisdevintcha@gmail.com>
2024-08-07 00:10:10 +0300

    Timer

---
+++ Assets/_Game/Adventure Game/Scripts/Timer.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using R3;
+using UnityEngine;
+
+namespace Adventure_Game.Scripts
+{
+    public class Timer: MonoBehaviour
+    {
+        public Observable<float> Time => _time;
+        private readonly ReactiveProperty<float> _time = new ();
+        private Coroutine _timerRoutine;
+
+        public void StartTimer()
+        {
+            _time.Value = 0;
+            if(_timerRoutine!= null) 
+                StopCoroutine(_timerRoutine);
+            _timerRoutine = StartCoroutine(TimerRoutine());
+        }
+
+        public float StopTimer()
+        {
+            StopCoroutine(_timerRoutine);
+            return _time.Value;
+        }
+
+        public float GetTime()
+        {
+            return _time.Value;
+        }
+
+        private IEnumerator TimerRoutine()
+        {
+            while (true)
+            {
+                _time.Value += UnityEngine.Time.deltaTime;
+                yield return null;
+            }
+        }
+    }
+}
